5b3373cf9eb78309aaebb845a5f03641dbdce3e5
Nabil Elhady <nabilelhady73@gmail.com>
2024-07-03 01:46:24 +0300

    2

--- client/src/widgets/Post.js
+++ client/src/widgets/Post.js
@@ -1,29 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// Sample data for blog posts
-
 const Home = ({ blogPosts }) => {
   return (
     <div className="">
       <h1 className="text-3xl font-bold mt-8 mb-4">Blog Posts</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {blogPosts.map((post) => (
-          <Link to={`/post/${post._id}`}>
-            <div
-              key={post.id}
-              className="hover:scale-125  bg-white shadow-lg rounded-lg p-4 hover:shadow-xl transition duration-300"
-            >
-              <h2 className="text-3xl font-bold text-green-600 mb-2 flex items-center justify-center">
+          <Link to={`/post/${post._id}`} key={post.id}>
+            <div className="bg-gray-100 mx-4 rounded-lg overflow-hidden shadow-xl hover:shadow-2xl transition duration-300 relative">
+              <h2 className="text-3xl font-bold text-green-600 mb-2 p-4 text-center">
                 {post.title}
               </h2>
-              <img
-                src={`http://localhost:3001/${post.cover}`} // Use the image URL from the post data
-                alt={`Image for ${post.title}`}
-                className="w-full h-auto rounded-md shadow-md mb-2"
-              />
-              <p className="text-gray-600">{post.date}</p>
-              <p className="text-gray-700 mt-2">{post.summary}</p>
+              <div className="relative overflow-hidden">
+                <img
+                  src={`http://localhost:3001/${post.cover}`}
+                  alt={`Image for ${post.title}`}
+                  className="w-full h-auto rounded-full p-4 shadow-md transition-transform duration-300 transform scale-100 hover:scale-125"
+                />
+              </div>
+              <p className="text-gray-600 p-4">{post.date}</p>
+              <p className="text-gray-700 px-4 pb-4 text-center">
+                {post.summary}
+              </p>
             </div>
           </Link>
         ))}
